daf7a315b540618aaa94f1885ed1d100c3eb8c39
stas115gr <stashauzkin10@gmail.com>
2024-11-04 19:45:39 +0200

    код

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Task 1
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={a + b}");
+            //Console.ReadKey();
+
+            ////Task 2
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={b} + {a}");
+            //Console.ReadKey();
+
+            ////Task 3
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{a:f1}*{b:f1}={a * b:f1}");
+            //Console.ReadKey();
+
+            ////Task 4
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f2}+{b:f2})+{c:f2}={a:f2}+({b:f2}+{c:f2})");
+            //Console.ReadKey();
+
+            ////Task 5
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.Write("d=");
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f2}/{b:f2}+{c:f2}/{d:f2}={a/b+c/d:f2}");
+            //Console.ReadKey();
+
+            ////Task 6
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"a+b={a}+{b}={a+b}");
+            //Console.Write($"  a-b={a}-{b}={a - b}");
+            //Console.Write($"  a*b={a}*{b}={a * b}");
+            //Console.ReadKey();
+
+            ////Task 7
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("a=");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"[a*(b+c)]=[(b+c)*a]:[{a}*({b}+{c})]=[({b}+{c})*{a}]={(b+c)*a}");
+            //Console.ReadKey();
+
+            ////Task 8
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("a=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f4}+({b:f4}+{c:f4}))=({a:f4}+{c:f4}+{b:f4})");
+            //Console.ReadKey();
+
+            ////Task 9
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{x*3}/{y}={x*3/y:f4}");
+            //Console.ReadKey();
+
+            ////Task 10
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"a*b={a:f3}*{b:f3}={a*b:f3}");
+            //Console.Write($"  a*b={a:f5}*{b:f5}={a*b:f5}");
+            //Console.ReadKey();
+        }
+    }
+}
